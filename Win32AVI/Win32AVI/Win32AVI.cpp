07fdc7d21f4c4d549bf23eb2feb5f2ff7638b797
Paul Lee <lihnux@hotmail.com>
2016-04-26 00:06:46 +0800

    Add a simple AVI operation, it can open the AVI File and show the first frame

--- Win32AVI/Win32AVI/Win32AVI.cpp
+++ Win32AVI/Win32AVI/Win32AVI.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 #include "Win32AVI.h"
+#include <Vfw.h>
+#include <Commdlg.h>
 
 #define MAX_LOADSTRING 100
 
@@ -17,6 +19,15 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+
+PAVIFILE aviFile = NULL;
+PAVISTREAM aviStream = NULL;
+PGETFRAME aviFrame = NULL;
+BITMAPINFOHEADER * bmpInfoHeader = NULL;
+
+BOOL InitAVIResource(PCTSTR fileName);
+void RemoveAVIResource();
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -26,6 +37,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: 在此放置代码。
+
+    // AVI File Initialize
+    AVIFileInit();
 
     // 初始化全局字符串
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -51,6 +65,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             DispatchMessage(&msg);
         }
     }
+
+    // AVI File Exit
+    AVIFileExit();
 
     return (int) msg.wParam;
 }
@@ -131,6 +148,34 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             // 分析菜单选择: 
             switch (wmId)
             {
+            case ID_FILE_OPEN:
+            {
+                OPENFILENAME ofn;       // common dialog box structure
+                TCHAR szFile[260];       // buffer for file name
+
+                // Initialize OPENFILENAME
+                ZeroMemory(&ofn, sizeof(ofn));
+                ofn.lStructSize = sizeof(ofn);
+                ofn.hwndOwner = hWnd;
+                ofn.lpstrFile = szFile;
+                // Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+                // use the contents of szFile to initialize itself.
+                ofn.lpstrFile[0] = _T('\0');
+                ofn.nMaxFile = sizeof(szFile);
+                ofn.lpstrFilter = _T("AVI\0*.*\0");
+                ofn.nFilterIndex = 1;
+                ofn.lpstrFileTitle = NULL;
+                ofn.nMaxFileTitle = 0;
+                ofn.lpstrInitialDir = NULL;
+                ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+                
+                if (GetOpenFileName(&ofn) == TRUE) {
+                    RemoveAVIResource();
+                    InitAVIResource(szFile);
+                }
+                InvalidateRect(hWnd, NULL, TRUE);
+            }
+                break;
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
@@ -147,6 +192,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 在此处添加使用 hdc 的任何绘图代码...
+            if (bmpInfoHeader != NULL) {
+                SetDIBitsToDevice(
+                    hdc, 0, 0, bmpInfoHeader->biWidth,
+                    bmpInfoHeader->biHeight,
+                    0, 0, 0, bmpInfoHeader->biHeight, bmpInfoHeader + 1,
+                    (BITMAPINFO *)bmpInfoHeader, DIB_RGB_COLORS
+                    );
+            }
             EndPaint(hWnd, &ps);
         }
         break;
@@ -177,4 +230,45 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     }
     return (INT_PTR)FALSE;
+}
+
+//AVI ファイルを開いて必要な初期化処理を行う
+BOOL InitAVIResource(PCTSTR fileName) {
+    //AVIFile を開く
+    if (AVIFileOpen(&aviFile, fileName, OF_READ, NULL)) {
+        return FALSE;
+    }
+    //AVI ストリームを取得する
+    if (AVIFileGetStream(aviFile, &aviStream, streamtypeVIDEO, 0)) {
+        AVIFileRelease(aviFile);
+        aviFile = NULL;
+        return FALSE;
+    }
+    //フレームを解凍するためのオブジェクトを取得
+    aviFrame = AVIStreamGetFrameOpen(aviStream, NULL);
+    if (aviFrame == NULL) {
+        AVIStreamRelease(aviStream);
+        AVIFileRelease(aviFile);
+        aviStream = NULL;
+        aviFile = NULL;
+        return FALSE;
+    }
+
+    bmpInfoHeader = (BITMAPINFOHEADER *)AVIStreamGetFrame(aviFrame, 10);
+    return TRUE;
+}
+
+//AVI ファイルに関連するすべてのリソースを解放する
+void RemoveAVIResource() {
+    //カレントフレームを NULL に設定して描画されないようにする
+    bmpInfoHeader = NULL;
+
+    //各リソースを解放
+    if (aviFrame) AVIStreamGetFrameClose(aviFrame);
+    if (aviStream) AVIStreamRelease(aviStream);
+    if (aviFile) AVIFileRelease(aviFile);
+
+    aviFrame = NULL;
+    aviStream = NULL;
+    aviFile = NULL;
 }
